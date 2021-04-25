1f9f42dec255a6f528455b1612c2919275988e60
Simon Meißner <simon.meissner1@gmx.net>
2021-04-25 18:53:01 +0200

    Task1

---
+++ main.js
@@ -0,0 +1,3 @@
+//divides the input array in divisions and sorts the returning lengths of divisions by lengths using teileUndBerrechneLaenge
+//a html table is created using erstelleTabelle
+erstelleTabelle(teileUndBerrechneLaenge(route).sort(function(a,b) { return a-b;}))
